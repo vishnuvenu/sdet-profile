513bab52bcecd4e4987b4dadb16b46cd7fcaba0d
vishnu venugopal <vishnuvenugopal73@gmail.com>
2025-06-20 12:43:01 +0530

    [venuvis]0 title change

--- sdet-profile/app/layout.tsx
+++ sdet-profile/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Vishnu Venugopal | SDET | Agile | AI',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
